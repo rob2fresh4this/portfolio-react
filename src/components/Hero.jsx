abf1c7aa12bd900ebe296e86cac3dc91164524fb
rob2fresh4this <grijrob07@gmail.com>
2025-05-02 18:05:46 -0700

    feat: enhance Hero component styling and add text outline utilities

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -41,11 +41,12 @@ const Hero = ({ navButton }) => {
                 muted
                 loop
                 playsInline
-                className="absolute inset-0 w-full h-full object-cover z-0"
+                className="absolute inset-0 w-full h-full object-cover object-left z-0"
             >
                 <source src={asabaharumasazzz} type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
+
             {/* Name */}
             <h1 className="text-4xl md:text-6xl font-extrabold text-white mb-4 relative z-10">
                 Robert A. Grijalva
@@ -67,22 +68,22 @@ const Hero = ({ navButton }) => {
                 </a>
                 <a
                     href="#contact"
-                    className="relative z-10 border border-white hover:bg-white hover:text-gray-900 text-white font-medium py-2 px-4 rounded-lg transition-all"
+                    className="relative z-10 borde bg-[#6b6c6b] hover:bg-[#565551] md:border-white md:hover:bg-white hover:text-gray-900 text-white font-medium py-2 px-4 rounded-lg transition-all"
                     onClick={toggleDetails1}
                 >
                     Contact Me
                 </a>
             </div>
 
-            <div className="relative w-[40%] min-h-[150px]">
+            <div className="relative w-[90%] md:w-[40%] min-h-[150px]">
                 {/* Projects */}
                 <div
                     className={`absolute inset-0 transition-all duration-700 ease-out
                     ${showDetails ? 'opacity-100 translate-y-0 pointer-events-auto' : 'opacity-0 translate-y-4 pointer-events-none'}
     `}
                 >
-                    <p className="mb-6 text-gray-100 font-bold">Projects I have worked on include:</p>
-                    <ul className="list-disc pl-6 text-gray-50 space-y-2">
+                    <p className="mb-6 text-gray-100 font-bold text-outline md:text-no-outline">Projects I have worked on include:</p>
+                    <ul className="list-disc pl-6 text-gray-50 space-y-2 text-outline md:text-no-outline">
                         <p className="font-bold">Study Buddys: <a href="https://github.com/davidmonterrosa/study-buddies-frontend" target="_blank" className="text-[#e8bf40] font-normal hover:font-bold">frontend</a></p>
                         <p className="font-bold">Study Buddys: <a href="https://github.com/rob2fresh4this/study-buddys-backend-v2" target="_blank" className="text-[#e8bf40] font-normal hover:font-bold">backend</a></p>
                         <p className="font-bold">Pokemon Next App: <a href="https://github.com/rob2fresh4this/pokemon-api-next-rebuild" target="_blank" className="text-[#e8bf40] font-normal hover:font-bold">frontend</a></p>
@@ -95,8 +96,8 @@ const Hero = ({ navButton }) => {
                     ${showDetails1 ? 'opacity-100 translate-y-0 pointer-events-auto' : 'opacity-0 translate-y-4 pointer-events-none'}
     `}
                 >
-                    <p className="mb-6 text-gray-100 font-bold">You can contact me via:</p>
-                    <ul className="list-disc pl-6 text-gray-50 space-y-2">
+                    <p className="mb-6 text-gray-100 font-bold text-outline md:text-no-outline">You can contact me via:</p>
+                    <ul className="list-disc pl-6 text-gray-50 space-y-2 text-outline md:text-no-outline">
                         <p className="font-bold">Email: <a href="mailto:grijrob07@gmail.com" className="font-normal text-[#e8bf40] hover:font-bold">My Email</a></p>
                         <p className="font-bold">GitHub: <a href="https://github.com/rob2fresh4this" target="_blank" className="font-normal text-[#e8bf40] hover:font-bold">My Github</a></p>
                         <p className="font-bold">Phone: <a href="tel:2095945363" className="font-normal text-[#e8bf40] hover:font-bold">My Phone</a></p>
